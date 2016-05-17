176735cfa8ff3d9dc4cd6f9323880e878fe419ee
pmazenauer1 <pmazenauer1@ntb.ch>
2016-05-17 16:38:37 +0200

    Test auf Nachbarn funktioniert.

--- hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/GraphDas.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/GraphDas.java
@@ -5,8 +5,10 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.Traverser;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Direction;
@@ -114,7 +116,7 @@ public class GraphDas {
 	}
 
 	private boolean needsBridge(Vertex node) {
-		return (long) node.property("bridges").value() >= node.graph().traversal().V(node).bothE("bridge").count()
+		return (int) node.property("bridges").value() >= node.graph().traversal().V(node).bothE("bridge").count()
 				.toList().get(0);
 	}
 
@@ -127,6 +129,14 @@ public class GraphDas {
 		vertices.addAll(g.traversal().V(node).repeat(__.in("column")).until(__.values("bridges").is(P.neq(0))).toSet());
 		vertices.addAll(
 				g.traversal().V(node).repeat(__.out("column")).until(__.values("bridges").is(P.neq(0))).toSet());
+		for (Vertex v : vertices) {
+			if (!areNeighbors(node, v)) {
+				System.out.println("Are Neighbors is wrong!");
+			}
+			if (!areNeighbors2(node, v)) {
+				System.out.println("Found Bridge Violation!");
+			}
+		}
 		return vertices;
 	}
 
@@ -144,6 +154,79 @@ public class GraphDas {
 		return bridges;
 	}
 
+	private boolean areNeighbors2(Vertex node1, Vertex node2) {
+		int x1 = (int) node1.property("x").value();
+		int y1 = (int) node1.property("y").value();
+		int x2 = (int) node2.property("x").value();
+		int y2 = (int) node2.property("y").value();
+		Graph g = node1.graph();
+		if (x1 != x2 && y1 != y2) {
+			return false;
+		} else {
+			GraphTraversal<Vertex, Vertex> tr = g.traversal().V(node1);
+			Predicate<Traverser<Vertex>> complexFilterColumn = new Predicate<Traverser<Vertex>>() {
+
+				@Override
+				public boolean test(Traverser<Vertex> t) {
+					List<Long> testList = g.traversal().V(t.get())
+							.match(__.<Vertex> as("t").repeat(__.in("row")).until(__.values("bridges").is(P.neq(0)))
+									.bothE("bridge").as("bridge"),
+									__.<Vertex> as("t").repeat(__.out("row")).until(__.values("bridges").is(P.neq(0)))
+											.bothE("bridge").as("bridge"))
+							.select("bridge").count().toList();
+					if (testList.size() > 0) {
+						if (testList.get(0) > 0) {
+							return false;
+						} else {
+							return true;
+						}
+					} else {
+						return true;
+					}
+				}
+			};
+			Predicate<Traverser<Vertex>> complexFilterRow = new Predicate<Traverser<Vertex>>() {
+
+				@Override
+				public boolean test(Traverser<Vertex> t) {
+					List<Long> testList = g.traversal().V(t.get())
+							.match(__.<Vertex> as("t").repeat(__.in("column")).until(__.values("bridges").is(P.neq(0)))
+									.bothE("bridge").as("bridge"),
+									__.<Vertex> as("t").repeat(__.out("column"))
+											.until(__.values("bridges").is(P.neq(0))).bothE("bridge").as("bridge"))
+							.select("bridge").count().toList();
+					if (testList.size() > 0) {
+						if (testList.get(0) > 0) {
+							return false;
+						} else {
+							return true;
+						}
+					} else {
+						return true;
+					}
+				}
+			};
+			if (x1 > x2) {
+				tr = tr.repeat(__.in("row").and(__.values("bridges").is(0)).filter(complexFilterRow))
+						.until(__.values("x").is(x1));
+			}
+			if (x1 < x2) {
+				tr = tr.repeat(__.out("row").and(__.values("bridges").is(0)).filter(complexFilterRow))
+						.until(__.values("x").is(x2));
+			}
+			if (y1 > y2) {
+				tr = tr.repeat(__.in("column").and(__.values("bridges").is(0)).filter(complexFilterColumn))
+						.until(__.values("y").is(y2));
+			}
+
+			if (y1 < y2) {
+				tr = tr.repeat(__.out("column").and(__.values("bridges").is(0)).filter(complexFilterColumn))
+						.until(__.values("y").is(y2));
+			}
+			return tr.toList().size() > 0;
+		}
+	}
+
 	private boolean areNeighbors(Vertex node1, Vertex node2) {
 		int x1 = (int) node1.property("x").value();
 		int y1 = (int) node1.property("y").value();
@@ -155,16 +238,26 @@ public class GraphDas {
 		} else {
 			GraphTraversal<Vertex, Vertex> tr = g.traversal().V(node1);
 			if (x1 > x2) {
-				tr = tr.repeat(__.in("row").and(__.values("bridges").is(0))).until(__.values("x").is(x2));
+				tr = tr.repeat(__.in("row").or(__.and(__.values("x").is(P.eq(x2)), __.values("bridges").is(P.neq(0))),
+						__.and(__.values("x").is(P.gt(x2)), __.values("bridges").is(0))))
+						.until(__.values("x").is(P.eq(x2)));
 			}
 			if (x1 < x2) {
-				tr = tr.repeat(__.out("row").and(__.values("bridges").is(0))).until(__.values("x").is(x2));
+				tr = tr.repeat(__.out("row").or(__.and(__.values("x").is(P.eq(x2)), __.values("bridges").is(P.neq(0))),
+						__.and(__.values("x").is(P.lt(x2)), __.values("bridges").is(0))))
+						.until(__.values("x").is(P.eq(x2)));
 			}
 			if (y1 > y2) {
-				tr = tr.repeat(__.in("column").and(__.values("bridges").is(0))).until(__.values("y").is(y2));
+				tr = tr.repeat(
+						__.in("column").or(__.and(__.values("y").is(P.eq(y2)), __.values("bridges").is(P.neq(0))),
+								__.and(__.values("y").is(P.gt(y2)), __.values("bridges").is(0))))
+						.until(__.values("y").is(P.eq(y2)));
 			}
 			if (y1 < y2) {
-				tr = tr.repeat(__.out("column").and(__.values("bridges").is(0))).until(__.values("y").is(y2));
+				tr = tr.repeat(
+						__.out("column").or(__.and(__.values("y").is(P.eq(y2)), __.values("bridges").is(P.neq(0))),
+								__.and(__.values("y").is(P.lt(y2)), __.values("bridges").is(0))))
+						.until(__.values("y").is(P.eq(y2)));
 			}
 			return tr.toList().size() > 0;
 		}
--- hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/TestGraph.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/TestGraph.java
@@ -1,5 +1,6 @@
 package ch.ntb.swehashisg.hashi.graph;
 
+import ch.ntb.swehashisg.hashi.model.GraphBridge;
 import ch.ntb.swehashisg.hashi.model.GraphField;
 
 public class TestGraph {
@@ -17,6 +18,19 @@ public class TestGraph {
 					System.out.println("Neighbor Y: " + neighbor.getY());
 				}
 			}
+
+			GraphField field1 = new GraphField(1, 2);
+			GraphField field2 = new GraphField(1, 4);
+
+			GraphBridge graphBridge1 = new GraphBridge(field1, field2);
+
+			GraphField field3 = new GraphField(0, 3);
+			GraphField field4 = new GraphField(4, 3);
+
+			GraphBridge graphBridge2 = new GraphBridge(field3, field4);
+
+			graphDas.addBridge(graphBridge1);
+			graphDas.addBridge(graphBridge2);
 
 			GraphDasFactory.closeGraphDas(graphDas);
 		} catch (Exception ex) {
