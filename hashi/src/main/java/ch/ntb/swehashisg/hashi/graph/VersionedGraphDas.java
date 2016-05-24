a3230d96008bfb1ff8aca5a7122e985c6e34d703
IC3Coffee <gabriel_ghenzi@email.com>
2016-05-24 16:03:36 +0200

    Ungetestete Version der Versionsverwaltung

--- hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/VersionedGraphDas.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/VersionedGraphDas.java
@@ -1,5 +1,8 @@
 package ch.ntb.swehashisg.hashi.graph;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 import ch.ntb.swehashisg.hashi.model.GraphBridge;
 import ch.ntb.swehashisg.hashi.model.GraphPlayField;
 
@@ -9,34 +12,73 @@ public class VersionedGraphDas extends AbstractGraphDas {
 		mGraphDas = gd;
 	}
 
-	public void undo()
+	public boolean undo()
 	{
-		
+		if(mIndex <= 0)
+			return false;
+		BridgeOperation bo = mListOperations.get(mIndex);
+		executeOperation(!bo.mIsAddingBridge,bo.mGraphBridge);
+		mIndex++;
+		return true;
 	}
-	public void redo()
+	public boolean redo()
 	{
-		
+		if(mIndex > mListOperations.size() ||
+		   mIndex <= 0)
+			return false;
+		--mIndex;
+		BridgeOperation bo = mListOperations.get(mIndex);
+		executeOperation(bo.mIsAddingBridge, bo.mGraphBridge);		
+		return true;
 	}
 	
 	@Override
 	public GraphPlayField getPlayField() {
-		// TODO Auto-generated method stub
-		return null;
+		return mGraphDas.getPlayField();
 	}
 
 	@Override
 	public void addBridge(GraphBridge bridge) {
-		// TODO Auto-generated method stub
-
+		removeOperationsOverIndex();
+		mListOperations.add( new BridgeOperation(true, bridge) );
+		redo();
+		
 	}
 
 	@Override
 	public void removeBridge(GraphBridge bridge) {
-		// TODO Auto-generated method stub
-
+		removeOperationsOverIndex();
+		mListOperations.add( new BridgeOperation(false, bridge) );
+		redo();
 	}
 	
+	// Private Methods -----------------------------------------------
+	private void removeOperationsOverIndex()
+	{
+		while(mListOperations.size() > mIndex) 
+			mListOperations.remove(mIndex + 1);
+	}
 	
-	// Private -----------------------------------------------
+	private void executeOperation(boolean addBridge, GraphBridge bridge)
+	{
+		if(addBridge)
+			mGraphDas.addBridge(bridge);
+		else
+			mGraphDas.removeBridge(bridge);
+	}
+	
+	// Private Variables ----------------------------------------------
 	private GraphDas mGraphDas;
+	private class BridgeOperation extends Object
+	{
+		public BridgeOperation(boolean isAddingBridge, GraphBridge argGraphBridge)
+		{
+			mIsAddingBridge = isAddingBridge;
+			mGraphBridge = argGraphBridge;
+		}
+		public boolean mIsAddingBridge;
+		public GraphBridge mGraphBridge;
+	}
+	private ArrayList<BridgeOperation> mListOperations;
+	private int mIndex;
 }
