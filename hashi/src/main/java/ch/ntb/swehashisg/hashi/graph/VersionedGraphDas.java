aa0e4a694588834eaaee1d1438249b12cbf4f24b
IC3Coffee <gabriel_ghenzi@email.com>
2016-06-01 16:00:00 +0200

    Überflüssige Undos removed

--- hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/VersionedGraphDas.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/graph/VersionedGraphDas.java
@@ -3,12 +3,18 @@ package ch.ntb.swehashisg.hashi.graph;
 import java.util.ArrayList;
 
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import ch.ntb.swehashisg.hashi.controller.MainWindowController;
 import ch.ntb.swehashisg.hashi.model.GraphBridge;
 import ch.ntb.swehashisg.hashi.model.GraphField;
 import ch.ntb.swehashisg.hashi.model.GraphPlayField;
 
 public class VersionedGraphDas extends GraphDas {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainWindowController.class);
+
 
 	private BaseGraphDas graphDas;
 	private ArrayList<GraphDasOperation> listOperations;
@@ -28,7 +34,7 @@ public class VersionedGraphDas extends GraphDas {
 		if (!canUndo()) {
 			throw new IllegalArgumentException("Nothing to undo!");
 		}
-
+		logger.debug("@VersionedGraphDas::undo();------------------------");
 		index--;
 		GraphDasOperation graphDasOperation = listOperations.get(index);
 		graphDasOperation.undo();
@@ -61,17 +67,25 @@ public class VersionedGraphDas extends GraphDas {
 	public void addBridge(GraphBridge bridge) {
 		graphDas.addBridge(bridge);
 		addOperation(new AddBridgeOperation(bridge, graphDas));
+		removeNewerOperation();
 	}
 
 	@Override
 	public void removeBridge(GraphBridge bridge) {
 		graphDas.removeBridge(bridge);
 		addOperation(new RemoveBridgeOperation(bridge, graphDas));
+		removeNewerOperation();
 	}
 
 	@Override
 	public boolean isCorrect() {
 		return graphDas.isCorrect();
+	}
+	
+	private void removeNewerOperation()
+	{
+		while(listOperations.size() > index)
+			listOperations.remove(index + 1);
 	}
 
 	private static interface GraphDasOperation {
@@ -91,6 +105,7 @@ public class VersionedGraphDas extends GraphDas {
 
 		@Override
 		public void undo() {
+			logger.debug("@AddBridgeOperation::undo();------------------------");
 			graphDas.removeBridge(graphBridge);
 		}
 
@@ -130,6 +145,7 @@ public class VersionedGraphDas extends GraphDas {
 
 		@Override
 		public void undo() {
+			logger.debug("@AddSolutionBridgeOperation::undo();------------------------");
 			graphDas.removeSolutionBridge(graphBridge);
 		}
 
