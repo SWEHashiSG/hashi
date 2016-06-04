90bf9b88e781aeffec2fbd6ad517c890adf7f8fc
Martin Messmer <martin.messmer@ntb.ch>
2016-06-04 14:32:56 +0200

    JavaDoc für MainHashi

--- hashi/src/main/java/ch/ntb/swehashisg/hashi/MainHashi.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/MainHashi.java
@@ -8,17 +8,35 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+/**
+ * Main class from the Hashi game. Starts the application and show it on the screen.
+ * @author Martin
+ *
+ */
 public class MainHashi extends Application {
 
+	/**
+	 * Window width and height which is needed for a good optic
+	 */
 	private int windowWidth = 520;
 	private int windowHeight = 500;
+	
+	/**
+	 * Main class which is called at the Programm start
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		launch(args);
 	}
 
+	/**
+	 * Start class called from the JavaFX.
+	 * Create a new Stage and add the main window.
+	 */
 	public void start(Stage stage) throws Exception {
 		MainWindowController mainWindow = new MainWindowController();
-		Scene scene = new Scene(mainWindow,windowHeight, windowHeight);
+		Scene scene = new Scene(mainWindow,windowWidth, windowHeight);
         scene.getStylesheets().add("/styles/styles.css");
 		stage.setTitle("Hashi from Team SWEHashiSG");
 		URL url = getClass().getResource("/images/Icon.jpg");
