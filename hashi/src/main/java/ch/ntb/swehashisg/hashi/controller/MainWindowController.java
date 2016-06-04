e197e05016f54dabd9c42f61cbbea79220242e9c
Martin Messmer <martin.messmer@ntb.ch>
2016-06-04 14:24:46 +0200

    JavaDoc für MainWindowController

--- hashi/src/main/java/ch/ntb/swehashisg/hashi/controller/MainWindowController.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/controller/MainWindowController.java
@@ -30,18 +30,45 @@ import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.WindowEvent;
 
+/**
+ * Controller class for the main window of the game. All buttons are allocated
+ * on this pane and also the game field. This controller handles all the user
+ * Inputs from the buttons and also keyboard inputs.
+ * 
+ * @author Martin
+ */
 public class MainWindowController extends AnchorPane {
 
 	private static final Logger logger = LoggerFactory.getLogger(MainWindowController.class);
 
+	/**
+	 * Description type for XML and JSon files used for the open- and save-file
+	 * dialog.
+	 */
 	private static final String XML_DESCRIPTION = "XML File";
 	private static final String JSON_DESCRIPTION = "JSon File";
 
+	/**
+	 * Controller of the game field which will be placed on this panel.
+	 */
 	private GameFieldController gameField;
+
+	/**
+	 * Data model of the game
+	 */
 	GraphDas graphDas;
 
+	/**
+	 * JavaFX Attributes from the FXML-File. pane where the game field will be
+	 * placed.
+	 */
 	@FXML
 	private Pane pane;
+
+	/**
+	 * JavaFX Attributes from the FXML-File. All buttons on the game field. Used
+	 * to enable and disable if the representing function is not available.
+	 */
 	@FXML
 	private Button buttonUndo;
 	@FXML
@@ -57,6 +84,10 @@ public class MainWindowController extends AnchorPane {
 	@FXML
 	private Button buttonRestart;
 
+	/**
+	 * Constructor of the MainWindowController. Loads the FXML-file and also
+	 * initialize the buttons.
+	 */
 	public MainWindowController() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
 		fxmlLoader.setRoot(this);
@@ -70,31 +101,47 @@ public class MainWindowController extends AnchorPane {
 		updateButtons(false, false);
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the undo button.
+	 */
 	@FXML
 	public void undo() {
 		gameField.undo();
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the redo button.
+	 */
 	@FXML
 	public void redo() {
 		gameField.redo();
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the restart button.
+	 */
 	@FXML
 	public void restart() {
 		gameField.restart();
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the show solution
+	 * button.
+	 */
 	@FXML
 	public void showSolution() {
 		gameField.showSolution();
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the check button.
+	 */
 	@FXML
 	public void check() {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Game Check");
-		if (gameField.isCorrect()){
+		if (gameField.isCorrect()) {
 			alert.setHeaderText("Your on the right way");
 		} else {
 			alert.setHeaderText("Sorry, but there are some errors in your Game");
@@ -102,6 +149,11 @@ public class MainWindowController extends AnchorPane {
 		alert.showAndWait();
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the save button. Open a
+	 * save file dialog were he can choose the designation and the file type.
+	 * Supports XML and JSon
+	 */
 	@FXML
 	public void save() {
 		if (graphDas == null) {
@@ -125,6 +177,11 @@ public class MainWindowController extends AnchorPane {
 		}
 	}
 
+	/**
+	 * User input. Called from GUI if the user clicks on the open button. Open a
+	 * file open dialog were he can choose which file he would load. Supports
+	 * XML and JSon If the user does not aboard the choosen game loads.
+	 */
 	@FXML
 	public void open() {
 		logger.debug("Open Clicked");
@@ -151,11 +208,25 @@ public class MainWindowController extends AnchorPane {
 		}
 	}
 
+	/**
+	 * adds the game field on this main window.
+	 * 
+	 * @param gameField
+	 *            to add on the main window
+	 */
 	private void addGameField(GameFieldController gameField) {
 		pane.getChildren().add(gameField);
 		updateButtons(graphDas);
 	}
 
+	/**
+	 * User input if the user clicks on the empty screen on the pane. Hidden
+	 * function to start the game editor mode. Only starts if user clicks with
+	 * mouse wheel on this pane. Shows user a dialog to choose the game size and
+	 * starts editor mode.
+	 * 
+	 * @param mouseEvent
+	 */
 	@FXML
 	public void clickedOnPane(MouseEvent mouseEvent) {
 		if (mouseEvent.getButton() == MouseButton.MIDDLE) {
@@ -184,6 +255,15 @@ public class MainWindowController extends AnchorPane {
 		}
 	}
 
+	/**
+	 * starts the editor mode. Creating new empty game with sizeX and sizeY.
+	 * show the empty game on this window.
+	 * 
+	 * @param sizeX
+	 *            horizontal size of new game
+	 * @param sizeY
+	 *            vertical size of new game
+	 */
 	private void startEditorMode(int sizeX, int sizeY) {
 		pane.getChildren().remove(gameField);
 		graphDas = GraphDasFactory.getEmptyGraphDas(sizeX, sizeY);
@@ -192,6 +272,13 @@ public class MainWindowController extends AnchorPane {
 		addGameField(gameField);
 	}
 
+	/**
+	 * User input when he try to close the window. if a game is loaded show him
+	 * a dialog to choose between aboard, save game or quit without saving.
+	 * 
+	 * @param event
+	 *            WindowEvent from stage
+	 */
 	public void closeRequest(WindowEvent event) {
 		if (graphDas == null) {
 			return;
@@ -214,22 +301,42 @@ public class MainWindowController extends AnchorPane {
 			// simple close Window
 		}
 	}
-	
-	void updateButtons(GraphDas graphDas){
-		if (graphDas != null){
+
+	/**
+	 * Update all buttons on the main window. Called every time if something has
+	 * changed on the model.
+	 * 
+	 * @param graphDas
+	 */
+	void updateButtons(GraphDas graphDas) {
+		if (graphDas != null) {
 			updateButtons(graphDas.canUndo(), graphDas.canRedo());
 		}
 	}
 
+	/**
+	 * Update all buttons on the main window. Called every time if something has
+	 * changed on the model.
+	 * 
+	 * @param graphDas
+	 */
 	private void updateButtons(boolean canUndo, boolean canRedo) {
 		buttonUndo.setDisable(!canUndo);
 		buttonRedo.setDisable(!canRedo);
 		buttonShowSolution.setDisable(graphDas == null);
-		buttonRestart.setDisable(graphDas== null);
+		buttonRestart.setDisable(graphDas == null);
 		buttonSave.setDisable(graphDas == null);
 		buttonCheck.setDisable(graphDas == null);
 	}
 
+	/**
+	 * User input if the user type something on the keyboard during this window
+	 * is in front. Some standard Shortcuts to save, open, undo and redo.
+	 * 
+	 * @param event
+	 *            KeyEvent form the keyboard.
+	 */
+	@FXML
 	public void onKeyPressed(KeyEvent event) {
 		if (event.getCode() == KeyCode.S & event.isControlDown()) {
 			logger.debug("CTRL + S pressed");
