19c52aeec06898d05fbee4d92aa0d5e3ae1ab9fa
Martin Messmer <martin.messmer@ntb.ch>
2016-06-04 12:24:03 +0200

    JavaDoc für GameTime mit einigen kleinen anpassungen

--- hashi/src/main/java/ch/ntb/swehashisg/hashi/controller/GameTime.java
+++ hashi/src/main/java/ch/ntb/swehashisg/hashi/controller/GameTime.java
@@ -3,45 +3,84 @@ package ch.ntb.swehashisg.hashi.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Timer to measure a time in seconds.
+ * 
+ * @author Joel
+ *
+ */
 public class GameTime {
 
 	private static final Logger logger = LoggerFactory.getLogger(GameTime.class);
 
+	/**
+	 * Factor to calculate from milliseconds to seconds
+	 */
 	private static final int MILLISEC_TO_SEC = 1000;
 
+	/**
+	 * system time, when the timer has been started in milliseconds
+	 */
 	private long startTime;
-	private long currentTime;
-	private long gameTime;
 
+	/**
+	 * system time, when the timer has been stopped in milliseconds
+	 */
+	private long stopTime;
+
+	/**
+	 * Used to see if timer is running.
+	 */
+	private boolean running;
+
+	/**
+	 * Constructor for GameTime. Initialize start, stop, and runningTime with
+	 * zero
+	 */
 	public GameTime() {
-		currentTime = 0;
+		stopTime = 0;
 		startTime = 0;
-		gameTime = 0;
+		running = false;
 	}
 
-	public void startTime() { // start the Gametime
+	/**
+	 * start the timer with save the actual system time to startTime.
+	 */
+	public void startTime() {
 		logger.debug("start Timer");
 		startTime = System.currentTimeMillis();
-
+		running = true;
 	}
 
-	public void resetTime() { // reset the GameTime
+	/**
+	 * reset the timer by set all back to zero
+	 */
+	public void resetTime() {
 		logger.debug("reset timer");
 		startTime = 0;
-		currentTime = 0;
+		stopTime = 0;
+		running = false;
 	}
 
-	public void stopTime() { // stop the GameTime
+	/**
+	 * stop the timer with save the actual system time to stopTime.
+	 */
+	public void stopTime() {
 		logger.debug("stop timer");
-		currentTime = System.currentTimeMillis();
+		stopTime = System.currentTimeMillis();
+		running = false;
 	}
 
-	public long getTime() { // return Gametime
-		gameTime = currentTime - startTime;
-		return gameTime / MILLISEC_TO_SEC;
+	/**
+	 * 
+	 * @return how long the timer has been running in seconds
+	 */
+	public long getTime() {
+		long runningTime = stopTime - startTime;
+		return runningTime / MILLISEC_TO_SEC;
 	}
 
 	public boolean isRunning() {
-		return (startTime == 0) != (currentTime == 0);
+		return running;
 	}
 }
